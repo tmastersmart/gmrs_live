f4dec0a14b57ac0622c318f780a9b17ecb5c3007
Tmaster <48128988+tmastersmart@users.noreply.github.com>
2023-07-05 00:53:56 -0500

    Update install.php

--- install.php
+++ install.php
@@ -153,16 +153,16 @@ function create_nodea ($file){
 global $file,$path,$tts;
 // phase 1 import node - call
 //$line= exec("cat /usr/local/etc/allstar_node_info.conf  |egrep 'NODE1='",$output,$return_var);
-$file ="/usr/local/etc/allstar_node_info.conf";
+$file ="/usr/local/etc/allstar_node_info.conf"; copy($file, "$path/allstar_node_info.conf");
 $fileIN= file($file);
 foreach($fileIN as $line){
 $line = str_replace("\r", "", $line);
 $line = str_replace("\n", "", $line);
 $line = str_replace('"', "", $line);
-$pos = strpos($line, 'ODE1=');
+$pos = strpos("-$line", 'NODE1=');
 if ($pos){$u= explode("=",$line);
 $node=$u[1];}
-$pos2 = strpos($line, 'CALL='); 
+$pos2 = strpos("-$line", 'STNCALL='); 
 if ($pos2){$u= explode("=",$line);
 $call=$u[1];}
 }
