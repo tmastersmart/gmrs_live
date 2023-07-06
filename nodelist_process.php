b9775c2d4ff536b910f0b318ce58f72c7128e78e
Tmaster <48128988+tmastersmart@users.noreply.github.com>
2023-07-06 16:28:38 -0500

    Update nodelist_process.php

--- nodelist_process.php
+++ nodelist_process.php
@@ -65,15 +65,19 @@ if ($u[0]==1000){// erase the header
     $u[0]= 0;
     }     
  }
- 
+
+//1985|Inactive
+//1986|Inactive
+//1987|Inactive
+//1988|Inactive
+//1989|Inactive
 $test= "-$u[1] $u[2]";
 // Unsure what this is remove it 
 $pos = strpos($test, "Inactive");if ($pos){$u[0]=0;}
  
    
-// node has to be over 400 
-if ($u[0]>410){  
-
+// 10 is blank 300 is active
+if ($u[0]>1){  
 // if no call then its a repeater or a HUB- Guess HUB
 if (!$u[3]){$u[4]="H";} 
 
@@ -91,14 +95,9 @@ $pos = strpos($test, "MOBILE");   if ($pos){$u[4]="N";}
 $pos = strpos($test, "DVswitch"); if ($pos){$u[4]="D";}
 $pos = strpos($test, "Emergency");if ($pos){$u[4]="H";}
 
-
-
-
 // Not repeaters but a hub
 if ($u[0] == 1195){$u[4]="H";}
  
-
-
 // These are known repeaters. that fail autodetect
 if ($u[0] == 1050){$u[4]="R";}
 if ($u[0] == 1051){$u[4]="R";}
@@ -117,27 +116,19 @@ if ($u[0] == 1236){$u[4]="R";}
 if ($u[0] == 1237){$u[4]="R";}
 if ($u[0] == 1238){$u[4]="R";}
 if ($u[0] == 1239){$u[4]="R";}
-
-
-
 if ($u[0] == 1513){$u[4]="R";}
-
 if ($u[0] == 1531){$u[4]="R";}
 if ($u[0] == 1532){$u[4]="R";}
 if ($u[0] == 1533){$u[4]="R";}
 if ($u[0] == 1534){$u[4]="R";}
-
 if ($u[0] == 1730){$u[4]="R";}
 if ($u[0] == 1731){$u[4]="R";}
 if ($u[0] == 1733){$u[4]="R";}
-
 if ($u[0] == 2341){$u[4]="R";}
 if ($u[0] == 2342){$u[4]="R";}
 
 
-
 // These dont look like hubs force NODE
-
 if ($u[0] == 1120){$u[4]="N";}
 if ($u[0] == 1121){$u[4]="N";}
 if ($u[0] == 1122){$u[4]="N";}
@@ -172,17 +163,37 @@ if ($u[0]==2978){$u[4]= "N";}// No named node?????
 // Texas GMRS data 2250 - 2267 are corrupted. 
 // This will auto fix. To Be removed.....
 // 1 is blank 2 contains the data that should  be in 1
-if ($u[1] ==""){ //slide over data to fix corruped entries
-    $u[1]=$u[2];
-    if($u[3]!=""){// Slide the next line
-      $u[2]=$u[3];
-      $u[3]="";
-    }
-}
 
- //  Corrupted data fix.   TBR when fixed
-if ($u[0]==1255){$u[1]="Carl"; $u[2]="San Antonio, TX USA"; $u[3]="[WRFH397]";$u[4]="N"; }
-  
+//2250||Texas GMRS Network - Statewide Link|
+//2251||North Texas Hub|
+//2252||South Texas Hub|
+//2253||East Texas Hub|
+//2254||West Texas Hub|
+//2255||Memorial Park 550 Repeater|
+//2256||Northwest Houston 725 Repeater|
+//2257||Channelview 675 Repeater|
+//2258||Dallas County REACT 675 Repeater|
+//2259||Lufkin 725 Repeater|
+//2260||Dickinson 650 Repeater|
+//2261||La Marque 700 Repeater|
+//2262||Montgomery 600 Repeater|
+//2263||Conroe 700 Repeater|
+//2264||La Grange 725 Repeater|
+//2265||Lubbock 700 Repeater|
+//2266||Sugar Land 600 Repeater|
+//2267||Chappell Hill 650 Repeater|
+//2268||Amarillo 650 Repeater|  
+
+//slide over data to fix corruped entries above
+if ($u[1] ==""){ $u[1]=$u[2];if($u[3]!=""){$u[2]=$u[3];$u[3]="";}}
+
+
+//1040|Thomas,- Hammond, IN|(WRCW750)
+//1041|Thomas,- Hammond, IN|(WRCW750)
+//1042|Thomas|- Hammond, IN|(WRCW750)
+//1043|Thomas|- Hammond, IN|(WRCW750)
+//1044|Thomas|- Hammond, IN|(WRCW750)
+
 // 1040 and 1041 have the wrong field have a , in place of a | 
 // TBR if fixed
 if ($u[0] ==1040){$u[1]="Thomas";$u[2]="Hammond, IN";$u[3]="[WRCW750]";$u[4]="N";}
@@ -190,13 +201,7 @@ if ($u[0] ==1041){$u[1]="Thomas";$u[2]="Hammond, IN";$u[3]="[WRCW750]";$u[4]="N"
 
 
 
- 
-
-
-
-
-
-// FIX IDs in the wrong fields 
+// FIX IDs in the wrong fields  ( this may all be fixed now?)
 $posL = strpos($u[1], "[W"); 
 if ($posL>=1){
     $test = explode("[",$u[1]);$id= explode("]",$test[1]);
@@ -241,8 +246,9 @@ if ($state[1]=="FLORIDA") {$state[1]="FL";}
 if ($state[1]=="NORTHCAROLINA") {$state[1]="NC";}
 if ($state[1]=="PENNSYLVANIA")  {$state[1]="PE";}
 if ($state[1]=="OHIO")  {  $state[1]="OH";}
-if ($state[1]=="OKLAHOMA")  {  $state[1]="OK";}
-if ($state[1]=="OREGON")  {  $state[1]="OR";}
+if ($state[1]=="OKLAHOMA"){$state[1]="OK";}
+if ($state[1]=="OREGON")  {$state[1]="OR";}
+if ($state[1]=="IDAHO")  { $state[1]="ID";}
 
 
 $test = str_replace(" ", "", $state[0]);
